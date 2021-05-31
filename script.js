91ee76fdccfc78f71225b01c95634aa98cd5f7c1
Mary Pirogova <pirogova.mariia@gmail.com>
2021-05-31 15:39:54 -0700

    created function which starts the quiz

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var timeEl = document.querySelector(".time");
 // Selects element by id
 var mainEl = document.getElementById("main");
 
-var secondsLeft = 2;
+var secondsLeft = 10;
 
 function setTime() {
   // Sets interval in variable
@@ -27,11 +27,24 @@ function sendMessage() {
   timeEl.textContent = " ";
   var imgEl = document.createElement("img");
   imgEl.setAttribute("src", "images/Thats-all.jpg");
-//   imgEl.setAttribute("width", "200px");
-//   imgEl.setAttribute("height", "200px");
   imgEl.classList.add("img");
   mainEl.appendChild(imgEl);
 
 }
 
-setTime();
+var btnStart = document.querySelector("#starts-quiz");
+
+btnStart.addEventListener("click", startQuiz);
+
+function startQuiz() {
+
+  //hides start block
+  var greetingsBlock = document.querySelector("#greetings");
+  greetingsBlock.classList.add("hidden");
+  //shows question block
+  var greetingsBlock = document.querySelector("#quiz-main");
+  greetingsBlock.classList.remove("hidden");
+
+  //starts timer
+  setTime();
+}
